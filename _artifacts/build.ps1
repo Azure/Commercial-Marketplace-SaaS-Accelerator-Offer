e6eff870ea06253174f397717c88df9385a0380b
SANTHOSH B <63607175+santhoshb-msft@users.noreply.github.com>
2024-02-07 15:30:38 -0800

    updated the build script

--- _artifacts/build.ps1
+++ _artifacts/build.ps1
@@ -1,10 +1,23 @@
- git clone https://github.com/Azure/Commercial-Marketplace-SaaS-Accelerator.git -b 7.5.1 --depth 1;
- cd ./Commercial-Marketplace-SaaS-Accelerator/deployment;
- dotnet publish ../src/AdminSite/AdminSite.csproj -c release -o ../Publish/AdminSite/
- dotnet publish ../src/MeteredTriggerJob/MeteredTriggerJob.csproj -c release -o ../Publish/AdminSite/app_data/jobs/triggered/MeteredTriggerJob/ -v q --runtime win-x64 --self-contained true
- dotnet publish ../src/CustomerSite/CustomerSite.csproj -c release -o ../Publish/CustomerSite/
- Compress-Archive -Path ../Publish/AdminSite/* -DestinationPath ../Publish/AdminSite.zip -Force
- Compress-Archive -Path ../Publish/CustomerSite/* -DestinationPath ../Publish/CustomerSite.zip -Force
+$saversionnumber = "7.5.1"
+$adminzip = "AdminSite751"
+$customerzip = "CustomerSite751"
+$ServerUri = ""
+$SQLDatabaseName = ""
+$SQLAdminLogin = ""
+$SQLServerName = ""
+$SQLAdminLoginPassword = ""		
+
+Remove-Item -Path _work -Recurse -Force -ErrorAction Ignore
+md _work 
+cd _work
+
+git clone https://github.com/Azure/Commercial-Marketplace-SaaS-Accelerator.git -b $saversionnumber --depth 1;
+cd ./Commercial-Marketplace-SaaS-Accelerator/deployment;
+dotnet publish ../src/AdminSite/AdminSite.csproj -c release -o ../Publish/AdminSite/
+dotnet publish ../src/MeteredTriggerJob/MeteredTriggerJob.csproj -c release -o ../Publish/AdminSite/app_data/jobs/triggered/MeteredTriggerJob/ -v q --runtime win-x64 --self-contained true
+dotnet publish ../src/CustomerSite/CustomerSite.csproj -c release -o ../Publish/CustomerSite/
+Compress-Archive -Path ../Publish/AdminSite/* -DestinationPath ../Publish/$adminzip.zip -Force
+Compress-Archive -Path ../Publish/CustomerSite/* -DestinationPath ../Publish/$customerzip.zip -Force
 
   ## Set Connectionstring
 $Connection="Data Source=tcp:"+$ServerUri+",1433;Initial Catalog="+$SQLDatabaseName+";User Id="+$SQLAdminLogin+"@"+$SQLServerName+".database.windows.net;Password="+$SQLAdminLoginPassword+";"
